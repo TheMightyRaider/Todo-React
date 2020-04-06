4a0e02ba2b45a025ef9cdde770771afa2ee4fd82
TheMightyRaider <sivasanjay7@gmail.com>
2020-04-06 15:52:16 +0530

    Added keypress event for adding Task

--- src/Component/Addtodo.js
+++ src/Component/Addtodo.js
@@ -2,26 +2,33 @@ import React from "react";
 
 class Addtodo extends React.Component {
   state = {
-    title: ""
+    title: "",
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({
-      title: e.target.value
+      title: e.target.value,
     });
   };
 
-  handleClick = e => {
-    this.props.addTask(this.state.title);
-    this.setState({
-      title: ""
-    });
+  handleClick = (e) => {
+    if (e.which == "13" || e.type == "click") {
+      this.props.addTask(this.state.title);
+      this.setState({
+        title: "",
+      });
+    }
   };
 
   render() {
     return (
       <div className="inputForm">
-        <input type="text" name="todo" onChange={this.handleChange}></input>
+        <input
+          type="text"
+          name="todo"
+          onChange={this.handleChange}
+          onKeyUp={this.handleClick}
+        ></input>
         <button name="submit" onClick={this.handleClick}>
           Add Task
         </button>
